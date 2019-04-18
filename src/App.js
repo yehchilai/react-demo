22d9fd03991db3bcf422b2430bb5c887c520e5a2
Yeh-chi Lai <5914167+yehchilai@users.noreply.github.com>
2019-04-18 16:46:54 -0700

    - add passing a method and its arguments

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
     otherState: 'Will merge when using setState'
   }
 
-  switchNameHandler = () =>{
+  switchNameHandler = (newName) =>{
     // console.log('Was clicked!');
     // Do not do this: this.state.persons[0].name = 'Marry';
     this.setState({
       persons: [
-        {name: 'Marry', age: 30},
+        {name: newName, age: 30},
         {name: 'Marc', age: 40},
         {name: 'Mike', age: 60}
       ]
@@ -30,10 +30,17 @@ class App extends Component {
       <div className="App">
         <h1>Hi, this is a react project</h1>
         <p>This is really working!!!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        {/* Not recommended because of less efficient */}
+        <button onClick={() => this.switchNameHandler('## MARK ##')}>Switch Name</button>
+
+        <Person name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}/>
+        {/* recommended using bind */}
+        <Person click={this.switchNameHandler.bind(this, 'MARK!!')}
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} 
+                age={this.state.persons[2].age}/>
       </div>
     );
   }
