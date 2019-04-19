cf0109e2f229c24ad17421c5f18d47830639fac3
Yeh-chi Lai <5914167+yehchilai@users.noreply.github.com>
2019-04-19 16:38:51 -0700

    - list and key - flexible lists

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: 'Mark', age: 30 },
-      { name: 'Marc', age: 40 },
-      { name: 'Mike', age: 50 }
+      { id: 'ldfjk', name: 'Mark', age: 30 },
+      { id: 'ewfew', name: 'Marc', age: 40 },
+      { id: 'opjpj', name: 'Mike', age: 50 }
     ],
     otherState: 'Will merge when using setState',
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // Do not do this: this.state.persons[0].name = 'Marry';
-    this.setState({
-      persons: [
-        { name: newName, age: 30 },
-        { name: 'Marc', age: 40 },
-        { name: 'Mike', age: 60 }
-      ]
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    // Always using updating state immutably
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    // (Alternative) const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+
+    const personsUpdate = [...this.state.persons];
+    personsUpdate[personIndex] = person;
+
+    this.setState({ persons: personsUpdate });
   }
 
   togglePersonHandler = () => {
@@ -36,7 +42,7 @@ class App extends Component {
     const personsUpdate = [...this.state.persons] // (ES6) use saperate operator. Copy before modify (immutably)
     // const personsUpdate = this.state.persons.slice()// (Old ) Optional 
     personsUpdate.splice(personIndex, 1);
-    this.setState({persons: personsUpdate});
+    this.setState({ persons: personsUpdate });
   }
 
   render() {
@@ -54,11 +60,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map( (person, index) => {
+          {this.state.persons.map((person, index) => {
             return <Person
-                    click = {() => this.deletePersonHandler(index)}
-                    name = {person.name}
-                    age = {person.age}/>
+              click={() => this.deletePersonHandler(index)}
+              changed={(event) => this.nameChangeHandler(event, person.id)}
+              name={person.name}
+              age={person.age}
+              key={person.id} />
           })}
         </div>
       );
