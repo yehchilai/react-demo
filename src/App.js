cf05368db05d82a629089fc3f6ef096e371fb8fd
Yeh-chi Lai <5914167+yehchilai@users.noreply.github.com>
2019-04-20 16:43:33 -0700

    - use ErrorBounday

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
 
@@ -54,12 +55,13 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              changed={(event) => this.nameChangeHandler(event, person.id)}
-              name={person.name}
-              age={person.age}
-              key={person.id} />
+            return <ErrorBoundary key={person.id}>
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+                name={person.name}
+                age={person.age}
+              /> </ErrorBoundary>
           })}
         </div>
       );
@@ -68,7 +70,7 @@ class App extends Component {
 
     // const classes = ['red', 'bold'].join(' '); // 'red bold'
     const assignedClasses = [];
-    
+
 
     if (this.state.persons.length <= 2) {
       assignedClasses.push(classes.red); // classes = ['red']
