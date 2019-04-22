c3ef8c9929bf32767496a8e24086bf739b6a18c2
Yeh-chi Lai <5914167+yehchilai@users.noreply.github.com>
2019-04-21 17:25:02 -0700

    - rendering adjancent JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css'
+import Aux from '../../../hoc/Auxiliary'
 
 class Person extends Component {
 
-    render(){
+    render() {
         console.log('[Person] rendering...');
-    return (
-        <div className={classes.Person}>
-            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>
-            <p>{this.props.children}</p>
-            <input type='text' onChange={this.props.changed} value={this.props.name}></input>
-        </div>
-    )
+
+        // (hoc) use high order component
+        return (
+            <Aux>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>
+                <p>{this.props.children}</p>
+                <input type='text' onChange={this.props.changed} value={this.props.name}></input>
+            </Aux>
+        )
+
+        // (Optional), use list with keys
+        // return [
+        //     <p key='item1' onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>,
+        //     <p key='item2'>{this.props.children}</p>,
+        //     <input key='item3' type='text' onChange={this.props.changed} value={this.props.name}></input>
+        // ]
+
+        // (Original)
+        // return (
+        //     <div className={classes.Person}>
+        //         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>
+        //         <p>{this.props.children}</p>
+        //         <input type='text' onChange={this.props.changed} value={this.props.name}></input>
+        //     </div>
+        // )
+
     }
-    
+
 }
 
 export default Person;
