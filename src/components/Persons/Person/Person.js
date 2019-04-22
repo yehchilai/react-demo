90a45798fe919d8eaf0c808118eff8661bb262bd
Yeh-chi Lai <5914167+yehchilai@users.noreply.github.com>
2019-04-22 16:37:40 -0700

    - using PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './Person.css'
 import Aux from '../../../hoc/Auxiliary'
 import withClass from '../../../hoc/withClassOther'
+import PropTypes from 'prop-types'
 
 class Person extends Component {
 
@@ -16,25 +17,15 @@ class Person extends Component {
                 <input type='text' onChange={this.props.changed} value={this.props.name}></input>
             </Aux>
         )
-
-        // (Optional), use list with keys
-        // return [
-        //     <p key='item1' onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>,
-        //     <p key='item2'>{this.props.children}</p>,
-        //     <input key='item3' type='text' onChange={this.props.changed} value={this.props.name}></input>
-        // ]
-
-        // (Original)
-        // return (
-        //     <div className={classes.Person}>
-        //         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>
-        //         <p>{this.props.children}</p>
-        //         <input type='text' onChange={this.props.changed} value={this.props.name}></input>
-        //     </div>
-        // )
-
     }
 
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+}; 
 
 export default withClass(Person, classes.Person);
