f5955d0854804bbda5491094887b2d3d829c3302
Yeh-chi Lai <5914167+yehchilai@users.noreply.github.com>
2019-04-21 17:29:25 -0700

    - using React.Fragment (the same with Aux)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.css'
 import Aux from '../../../hoc/Auxiliary'
 
@@ -7,13 +7,13 @@ class Person extends Component {
     render() {
         console.log('[Person] rendering...');
 
-        // (hoc) use high order component
+        // (hoc) use high order component (Fragment or React.Fragment)
         return (
-            <Aux>
+            <Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>
                 <p>{this.props.children}</p>
                 <input type='text' onChange={this.props.changed} value={this.props.name}></input>
-            </Aux>
+            </Fragment>
         )
 
         // (Optional), use list with keys
